6a0e59019404ea9ea81a3d05d6e32ad133a74129
Benedikt Caspari <b.caspari@consyd.de>
2018-03-23 11:10:53 +0100

    added eslint

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
-const fs = require("fs");
 require("dotenv").config();
 const jsonfile = require("jsonfile");
-const fileName = "./messageFiles/welcome.json";
+const welcomeMessageFile = "./messageFiles/welcome.json";
 const configFile = "./configuration.json";
 
 // Create an instance of a Discord client
@@ -11,84 +10,27 @@ const client = new Discord.Client();
 // The token of your bot - https://discordapp.com/developers/applications/me
 const token = process.env.BOTTOKEN;
 
-let roleMessages = [];
-let config = null;
+let botMessages = [];
+let botConfig = null;
+
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
 client.on("ready", () => {
-  jsonfile.readFile(fileName, function(err, obj) {
+  jsonfile.readFile(welcomeMessageFile, function(err, obj) {
     if (err) {
       console.log("No file");
     } else {
-      roleMessages = obj;
-      jsonfile.readFile(configFile, function(err, obj) {
-        if (err) {
-          console.log("No config file");
-        } else {
-          config = obj;
-          for (const configGuild of config.yourServerNames) {
-            const guild = client.guilds.find("name", configGuild);
-            if (guild) {
-              for (const welcomeText of config.welcomeChannelMessages) {
-                for (const role of welcomeText.roles) {
-                  const roleExist = guild.roles.find("name", role.roleName);
-                  if (!roleExist) {
-                    guild.createRole({
-                      name: role.roleName,
-                      color: role.color
-                    });
-                  }
-                }
-              }
-              const channels = guild.channels;
-              const welcomeChannel = channels.find(
-                "name",
-                config.welcomeChannelName
-              );
-              if (welcomeChannel) {
-                fetchMessages(welcomeChannel);
-                welcomeChannel.fetchPinnedMessages().then(stickies => {
-                  if (stickies) {
-                    let roleMessageIds = getRoleMessageIds(guild.id);
-                    if (
-                      !roleMessageIds ||
-                      !stickies.find("id", roleMessageIds[0])
-                    ) {
-                      for (const msg of config.welcomeChannelMessages) {
-                        sendWelcomeMessage(welcomeChannel, msg);
-                      }
-                    }
-                  }
-                });
-              } else {
-                guild
-                  .createChannel(config.welcomeChannelName)
-                  .then(newWelcomeChannel => {
-                    for (const msg of config.welcomeChannelMessages) {
-                      sendWelcomeMessage(newWelcomeChannel, msg);
-                    }
-                  });
-              }
-            } else {
-              console.log(
-                "Can't find " +
-                  configGuild +
-                  "! Check if configuration.json is configured properly and Bot is assigend to this Server"
-              );
-            }
-          }
-          console.log("I am ready!");
-        }
-      });
+      botMessages = obj;
+      readConfig();
     }
   });
 });
 
-client.on("guildDelete", guild => {
-  roleMessages.forEach((ele, index) => {
+client.on("guildDelete", (guild) => {
+  botMessages.forEach((ele, index) => {
     if (ele.guildId === guild.id) {
-      roleMessages.splice(index, 1);
-      jsonfile.writeFile(fileName, roleMessages, err => {
+      botMessages.splice(index, 1);
+      jsonfile.writeFile(welcomeMessageFile, botMessages, (err) => {
         if (err) {
           console.error(err);
         }
@@ -97,20 +39,23 @@ client.on("guildDelete", guild => {
     }
   });
 });
-const fetchMessages = channel => {
-  channel
-    .fetchMessages()
-    .then(cachedMessages => {
-      cachedMessages.forEach(function(cmsg, csmgId) {
-        cmsg.reactions.forEach(function(reaction, reactionId) {
-          reaction.fetchUsers();
-        });
+
+client.on("channelDelete", (channel) => {
+  botMessages.forEach((ele, index) => {
+    if (ele.guildId === channel.guild.id) {
+      botMessages.splice(index, 1);
+      jsonfile.writeFile(welcomeMessageFile, botMessages, (err) => {
+        if (err) {
+          console.error(err);
+        }
+        return;
       });
-    })
-    .catch(console.error);
-};
-const getRoleMessageIds = guildId => {
-  for (const msg of roleMessages) {
+    }
+  });
+});
+
+const getRoleMessageIds = (guildId) => {
+  for (const msg of botMessages) {
     if (msg.guildId === guildId) {
       return msg.messages;
     }
@@ -119,7 +64,7 @@ const getRoleMessageIds = guildId => {
 };
 
 const roleMessageExist = (guildId, messageId) => {
-  for (const msg of roleMessages) {
+  for (const msg of botMessages) {
     if (msg.guildId === guildId) {
       for (const msgId of msg.messages) {
         if (messageId === msgId) {
@@ -132,12 +77,12 @@ const roleMessageExist = (guildId, messageId) => {
 };
 
 const sendWelcomeMessage = (welcomeChannel, msg) => {
-  welcomeChannel.send(msg.text).then(newMessage => {
-    newMessage.pin().then(myMessage => {
+  welcomeChannel.send(msg.text).then((newMessage) => {
+    newMessage.pin().then((myMessage) => {
       let found = false;
       let guildFound = false;
       let messageToAddGuild = null;
-      for (const roleMsg of roleMessages) {
+      for (const roleMsg of botMessages) {
         if (roleMsg.guildId === myMessage.guild.id) {
           guildFound = true;
           messageToAddGuild = roleMsg;
@@ -151,7 +96,7 @@ const sendWelcomeMessage = (welcomeChannel, msg) => {
       }
       if (!found) {
         if (!guildFound) {
-          roleMessages.push({
+          botMessages.push({
             messages: [myMessage.id],
             guildId: welcomeChannel.guild.id
           });
@@ -159,7 +104,7 @@ const sendWelcomeMessage = (welcomeChannel, msg) => {
           messageToAddGuild.messages.push(myMessage.id);
         }
       }
-      jsonfile.writeFile(fileName, roleMessages, err => {
+      jsonfile.writeFile(welcomeMessageFile, botMessages, (err) => {
         if (err) {
           console.error(err);
         }
@@ -172,7 +117,7 @@ const sendWelcomeMessage = (welcomeChannel, msg) => {
 };
 
 // Create an event listener for messages
-client.on("message", message => {
+client.on("message", (message) => {
   if (message.author.bot) {
     return;
   }
@@ -187,17 +132,25 @@ client.on("message", message => {
     if (num && num > 0) {
       const ids = getRoleMessageIds(message.guild.id);
       const channels = message.guild.channels;
-      const welcomeChannel = channels.find("name", config.welcomeChannelName);
+      const welcomeChannel = channels.find(
+        "name",
+        botConfig.welcomeChannelName
+      );
       if (welcomeChannel) {
-        welcomeChannel.fetchPinnedMessages().then(stickies => {
+        welcomeChannel.fetchPinnedMessages().then((stickies) => {
           if (stickies) {
             const stickyMsg = stickies.find("id", ids[num - 1]);
             if (stickyMsg) {
               stickyMsg
                 .edit(str.substr(str.indexOf("]") + 1, str.length - 1))
-                .then(msg => {
+                .then((msg) => {
                   console.log(`New message content: ${msg}`);
-                  fetchMessages(welcomeChannel);
+                  welcomeChannel
+                    .fetchMessages()
+                    .then((cachedMessages) =>
+                      console.log(`Received ${messages.size} messages`)
+                    )
+                    .catch(console.error);
                 })
                 .catch(console.error);
             }
@@ -206,7 +159,7 @@ client.on("message", message => {
       }
     }
   }
-  for (const msg of config.serverMessages) {
+  for (const msg of botConfig.serverMessages) {
     if (str === msg.reactMessage) {
       // send answer from configuration json
       message.channel.send(msg.botAnswer);
@@ -225,12 +178,12 @@ client.on("messageReactionAdd", (reaction, user) => {
     roleMessageExist(reaction.message.guild.id, reaction.message.id)
   ) {
     const roleUser = reaction.message.guild.members.get(user.id);
-    for (const welcomeText of config.welcomeChannelMessages) {
+    for (const welcomeText of botConfig.welcomeChannelMessages) {
       for (const role of welcomeText.roles) {
         if (role.icon === reaction.emoji.name) {
           roleUser.addRole(roleUser.guild.roles.find("name", role.roleName));
           const msgText = role.roleSetText.replace("[username]", user.username);
-          reaction.message.channel.send(msgText).then(msg => {
+          reaction.message.channel.send(msgText).then((msg) => {
             msg.delete(15000);
           });
         }
@@ -245,7 +198,7 @@ client.on("messageReactionRemove", (reaction, user) => {
     roleMessageExist(reaction.message.guild.id, reaction.message.id)
   ) {
     const roleUser = reaction.message.guild.members.get(user.id);
-    for (const welcomeText of config.welcomeChannelMessages) {
+    for (const welcomeText of botConfig.welcomeChannelMessages) {
       for (const role of welcomeText.roles) {
         if (role.icon === reaction.emoji.name) {
           roleUser.removeRole(roleUser.guild.roles.find("name", role.roleName));
@@ -253,7 +206,7 @@ client.on("messageReactionRemove", (reaction, user) => {
             "[username]",
             user.username
           );
-          reaction.message.channel.send(msgText).then(msg => {
+          reaction.message.channel.send(msgText).then((msg) => {
             msg.delete(15000);
           });
         }
@@ -261,6 +214,81 @@ client.on("messageReactionRemove", (reaction, user) => {
     }
   }
 });
+
+const checkGuildRoles = (guild) => {
+  for (const welcomeText of botConfig.welcomeChannelMessages) {
+    for (const role of welcomeText.roles) {
+      const roleExist = guild.roles.find("name", role.roleName);
+      if (!roleExist) {
+        guild.createRole({
+          name: role.roleName,
+          color: role.color
+        });
+      }
+    }
+  }
+};
+
+const readConfig = () => {
+  jsonfile.readFile(configFile, function(err, obj) {
+    if (err) {
+      console.log("No config file");
+    } else {
+      botConfig = obj;
+      for (const configGuild of botConfig.yourServerNames) {
+        const guild = client.guilds.find("name", configGuild);
+        if (guild) {
+          checkGuildRoles(guild);
+          const channels = guild.channels;
+          const welcomeChannel = channels.find(
+            "name",
+            botConfig.welcomeChannelName
+          );
+          if (welcomeChannel) {
+            welcomeChannel
+              .fetchPinnedMessages()
+              .then((cachedMessages) => {
+                cachedMessages.forEach(function(cmsg, csmgId) {
+                  cmsg.reactions.forEach(function(reaction, reactionId) {
+                    reaction.fetchUsers();
+                  });
+                });
+              })
+              .catch(console.error);
+            welcomeChannel.fetchPinnedMessages().then((stickies) => {
+              if (stickies) {
+                let roleMessageIds = getRoleMessageIds(guild.id);
+                if (
+                  !roleMessageIds ||
+                  !stickies.find("id", roleMessageIds[0])
+                ) {
+                  for (const msg of botConfig.welcomeChannelMessages) {
+                    sendWelcomeMessage(welcomeChannel, msg);
+                  }
+                }
+              }
+            });
+          } else {
+            guild
+              .createChannel(botConfig.welcomeChannelName)
+              .then((newWelcomeChannel) => {
+                for (const msg of botConfig.welcomeChannelMessages) {
+                  sendWelcomeMessage(newWelcomeChannel, msg);
+                }
+              });
+          }
+        } else {
+          console.log(
+            "Can't find " +
+              configGuild +
+              "! Check if configuration.json is configured properly and Bot is assigend to this Server"
+          );
+        }
+      }
+      console.log("I am ready!");
+    }
+  });
+};
 
 // Log our bot in
 client.login(token);
